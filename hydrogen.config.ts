983ad57968e61d2f206d0d929f509c2d1699d0d4
SnowRobert <snowrobert0512@gmail.com>
2023-05-07 23:13:19 -0400

    deploy

--- hydrogen.config.ts
+++ hydrogen.config.ts
@@ -4,8 +4,8 @@ export default defineConfig({
   shopify: {
     defaultCountryCode: 'US',
     defaultLanguageCode: 'EN',
-    storeDomain: import.meta.env.PUBLIC_STORE_DOMAIN,
-    storefrontToken: import.meta.env.PUBLIC_STOREFRONT_API_TOKEN,
+    storeDomain: import.meta.env.PUBLIC_STORE_DOMAIN || '74c56a-4.myshopify.com',
+    storefrontToken: import.meta.env.PUBLIC_STOREFRONT_API_TOKEN || '8e45cda33e4581ac81f30dbb100ac171',
     storefrontApiVersion: '2023-04',
   },
   session: CookieSessionStorage('__session', {
