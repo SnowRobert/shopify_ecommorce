d0b4c8e755cf74f83b55010785eb2beeb6ab5ee4
SnowRobert <snowrobert0512@gmail.com>
2023-05-09 04:24:44 -0400

    vite

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,18 @@
-import { defineConfig } from "vite";
+import {defineConfig} from 'vite';
+import hydrogen from '@shopify/hydrogen/plugin';
+
 export default defineConfig({
   base: "/shopify_ecommorce/",
+  plugins: [hydrogen()],
+  resolve: {
+    alias: [{find: /^~\/(.*)/, replacement: '/src/$1'}],
+  },
+  optimizeDeps: {
+    include: ['@headlessui/react', 'clsx', 'react-use', 'typographic-base'],
+  },
+  test: {
+    globals: true,
+    testTimeout: 10000,
+    hookTimeout: 10000,
+  },
 });
